ee412a987b7b479cef6e4a1603fd51f9613e4823
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-06-13 18:27:10 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,67 +1,67 @@
 let config = {};
 
-async function loadConfig(){
+async function loadConfig() {
 
-const response =
-await fetch("data/config.json");
+    const response =
+        await fetch("data/config.json");
 
-config = await response.json();
+    config =
+        await response.json();
 
-renderConfig();
-
-}
-
-function renderConfig(){
-
-document.getElementById("masjidName")
-.innerText =
-config.masjid.nama;
-
-document.getElementById("masjidAddress")
-.innerText =
-config.masjid.alamat;
-
-document.getElementById("logo")
-.src =
-config.masjid.logo;
-
-document.getElementById("runningText")
-.innerText =
-config.runningText.join(" • ");
-
-document.getElementById("announcement")
-.innerHTML =
-config.announcement
-.map(item=>`<p>${item}</p>`)
-.join("");
+    renderConfig();
 
 }
 
-function updateClock(){
+function renderConfig() {
 
-const now = new Date();
+    document.getElementById("masjidName")
+        .innerText =
+        config.masjid.nama;
 
-document.getElementById("clock")
-.innerText =
-now.toLocaleTimeString("id-ID");
+    document.getElementById("masjidAddress")
+        .innerText =
+        config.masjid.alamat;
 
-document.getElementById("date")
-.innerText =
-now.toLocaleDateString("id-ID",{
-weekday:"long",
-day:"numeric",
-month:"long",
-year:"numeric"
-});
+    document.getElementById("logo")
+        .src =
+        config.masjid.logo;
+
+    document.getElementById("runningText")
+        .innerText =
+        config.runningText.join(" • ");
+
+    document.getElementById("announcement")
+        .innerHTML =
+        config.announcement
+        .map(item => `<p>${item}</p>`)
+        .join("");
 
 }
 
-setInterval(updateClock,1000);
+function updateClock() {
 
-loadConfig();
+    const now = new Date();
+
+    document.getElementById("clock")
+        .innerText =
+        now.toLocaleTimeString("id-ID");
+
+    document.getElementById("date")
+        .innerText =
+        now.toLocaleDateString("id-ID", {
+            weekday: "long",
+            day: "numeric",
+            month: "long",
+            year: "numeric"
+        });
+
+}
+
+setInterval(updateClock, 1000);
+
 updateClock();
 
-loadConfig().then(()=>{
+loadConfig().then(() => {
 
     loadPrayerTimes();
 
