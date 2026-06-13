2e929f972f40642d0098283f1d5189f197139aaa
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-06-13 14:37:06 +0700

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,62 @@
+let config = {};
+
+async function loadConfig(){
+
+const response =
+await fetch("data/config.json");
+
+config = await response.json();
+
+renderConfig();
+
+}
+
+function renderConfig(){
+
+document.getElementById("masjidName")
+.innerText =
+config.masjid.nama;
+
+document.getElementById("masjidAddress")
+.innerText =
+config.masjid.alamat;
+
+document.getElementById("logo")
+.src =
+config.masjid.logo;
+
+document.getElementById("runningText")
+.innerText =
+config.runningText.join(" • ");
+
+document.getElementById("announcement")
+.innerHTML =
+config.announcement
+.map(item=>`<p>${item}</p>`)
+.join("");
+
+}
+
+function updateClock(){
+
+const now = new Date();
+
+document.getElementById("clock")
+.innerText =
+now.toLocaleTimeString("id-ID");
+
+document.getElementById("date")
+.innerText =
+now.toLocaleDateString("id-ID",{
+weekday:"long",
+day:"numeric",
+month:"long",
+year:"numeric"
+});
+
+}
+
+setInterval(updateClock,1000);
+
+loadConfig();
+updateClock();
